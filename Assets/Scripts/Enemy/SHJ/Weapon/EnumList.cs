ddde99c5b689ba42dbb0b2e45ff186a281212602
Sung-27 <shj_27@naver.com>
2025-12-11 10:07:49 +0900

    에너미(성혁준)

--- Assets/Scripts/Enemy/SHJ/Weapon/EnumList.cs
+++ Assets/Scripts/Enemy/SHJ/Weapon/EnumList.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public static class EnumList
 {
-    public enum AttackName
+    public enum AttackType
     {
         None,
-        stirng
+        Slash,          // 근접 기본
+        Stab,           // 찌르기
+        SpinAttack,     // 회전베기
+
+        Shoot,          // 원거리 기본
+        ArrowRain,      // 화살 비
+        Fireball,       // 파이어볼
+
+        Charge,         // 돌진 공격
+        Grenade,        // 수류탄
+        Laser           // 레이저
+        // 나중에 100개가 돼도 여기만 추가하면 끝!
     }
 
 }
--- Assets/Scripts/Enemy/SHJ/Weapon/WeaponObject.cs
+++ Assets/Scripts/Enemy/SHJ/Weapon/WeaponObject.cs
@@ -7,14 +7,19 @@ using UnityEngine;
 public class WeaponObject : ScriptableObject
 {
 
+
     
-    public ActtackManager.AttackRange attackRange;
-    
+
 
 
     [Header("기타 무기 정보")]
-    public AttackName
+
     public float damage;        //데미지
     public float attackDelay;   //공격딜레이
     public float HitLength;     //공격히트
+
+    public ActtackManager.AttackRange attackRange;
+
+    [Header("이 무기가 사용할 기본 공격")]
+    public EnumList.AttackType defaultAttack;   // 이거 하나만 지정!
 }
