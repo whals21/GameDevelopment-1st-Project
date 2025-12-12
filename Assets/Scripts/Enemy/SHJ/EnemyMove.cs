84a8ebfa8516539e6070444476b86f679505d5ab
whals21 <dmadkrdmadkr1@gmail.com>
2025-12-12 22:26:05 +0900

    성혁준 브랜치와 병합에서 에러고친 버전_조민희

--- Assets/Scripts/Enemy/SHJ/EnemyMove.cs
+++ Assets/Scripts/Enemy/SHJ/EnemyMove.cs
@@ -20,8 +20,6 @@ public class EnemyMove : MonoBehaviour
 
     private void Move()
     {
-        
-
         if (main.Target == null)
         {
             main.rb.velocity = Vector2.zero;
@@ -29,15 +27,12 @@ public class EnemyMove : MonoBehaviour
         }
 
         Vector2 direction = (main.Target.position - transform.position).normalized;
-
         
         main.rb.velocity = direction * main.Data.moveSpeed;
 
-       
         if (direction.x > 0.01f)
             main.sr.flipX = false;
         else if (direction.x < -0.01f)
             main.sr.flipX = true;
-
     }
 }
